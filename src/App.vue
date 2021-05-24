95b63cab79d789047fd38465c78a0c4b9b6efca3
Shelly Tansil <shellytansil93@gmail.com>
2021-05-24 23:27:47 +0700

    import axios

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@
 </template>
 
 <script setup>
-import HelloWorld from './components/Todo.vue'
+import Todo from './components/Todo.vue'
+import axios from 'axios'
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
@@ -14,7 +15,6 @@ import HelloWorld from './components/Todo.vue'
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
