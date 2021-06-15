a66221563fc851c638b55d4f66a25a7c67373e1d
Shelly Tansil <shellytansil93@gmail.com>
2021-06-15 16:44:55 +0700

    add user and auth

---
+++ src/components/Auth.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h1>Authenticate</h1>
+    <div>
+      <label>Username: </label>
+      <input type="text" v-model="username"/>
+    </div>
+    <br />
+    <div>
+      <label>Password: </label>
+      <input type="password" v-model="password"/>
+    </div>
+    <br />
+    <button @click="auth">Log In</button> &nbsp;
+    <button @click="del">Log Out</button>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios'
+
+  export default{
+    data: function() {
+      return{
+        username: '',
+        password: ''
+      }
+    },
+    created: function(){
+      this.username = localStorage.getItem('usr')
+      this.password = localStorage.getItem('pwd')
+    },
+    methods: {
+      auth: function(){
+        localStorage.setItem('usr', this.username)
+        localStorage.setItem('pwd', this.password)
+      },
+      del: function(){
+        localStorage.removeItem('usr')
+        localStorage.removeItem('pwd')
+        this.username = ''
+        this.password = ''
+      }
+    }
+  }
+</script>
---
+++ src/components/User.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <h1>Berikut adalah daftar semua pengguna kita: </h1>
+    <input v-model="username" />
+    <input v-model="password" type="password"/>
+    <button @click="add">Add</button>
+    <ul>
+      <li v-for="(item) in users" v-bind:key="item._id">
+        {{ item.username }} 
+        <button @click="del(item._id)">X</button>  
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+  import axios from 'axios'
+
+  const username = localStorage.getItem('usr')
+  const password = localStorage.getItem('pwd')
+  const headers = { headers: { username, password } }
+
+  export default {
+    data: function(){
+      return{
+        users: [],
+        username: '',
+        password: ''
+      }
+    },
+    created: function(){
+      axios.get('http://localhost:3000/user', headers)
+      .then(result => {
+        this.users = result.data
+        console.log("rendering")
+      })
+    },
+    methods: {
+      add: function(){
+        let newUser = { username: this.username, password: this.password }
+        axios.post('http://localhost:3000/user', newUser, headers)
+        .then(result => {
+          console.log(result)
+          this.users.push({ _id: result.data._id, username: result.data.username })
+        })
+      },
+      del: function(id){
+        axios.delete(`http://localhost:3000/user/${id}`, headers)
+        .then(result => {
+          let idx = this.users.findIndex(i => i._id === id)
+          this.users.splice(idx, 1)
+        })
+      }
+    }
+  }
+</script>
